b00dfb5ad461c61d5037950ab1c4fd2400fcc176
Pratik Shah <psh@narola.email>
2022-11-02 16:17:37 +0530

    Configure dev environment file

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     hmr: false,
-    digioEnv: "production",
-    API_URL: "http://219.90.67.8:93/"
+    digioEnv: "sandbox",
+    API_URL: "http://dev.api.com"
 };
